d2015d3db791c626a1914363c6c3089ddb360c8f
Vikram Singh <113547823+Viku69@users.noreply.github.com>
2025-05-23 09:20:38 +0530

    Updated

--- routers/orders.py
+++ routers/orders.py
@@ -7,52 +7,6 @@ import uuid
 router = APIRouter()
 
 
-# @router.post("/order/checkout")
-# def checkout(user_id: int , store_id: int):
-#     conn = get_db_connection()
-#     cursor = conn.cursor()
-#     try:
-#         # 1. Fetch cart items with product prices
-#         cursor.execute("""
-#             SELECT p.id, p.price, c.quantity
-#             FROM cart c
-#             JOIN products p ON c.product_id = p.id
-#             WHERE c.user_id = %s
-#         """, (user_id,))
-#         items = cursor.fetchall()
-
-#         if not items:
-#             raise HTTPException(status_code=400, detail="Cart is empty")
-
-#         # 2. Calculate total
-#         total_amount = sum(item[1] * item[2] for item in items)
-
-#         # ✅ 3. Generate UUID for order_id in Python
-#         order_uuid = str(uuid.uuid4())
-
-
-#         # 4. Insert order and get generated ID
-#         cursor.execute("INSERT INTO orders (user_id, order_id, total_amount, store_id) VALUES (%s, %s, %s , %s)",
-#                        (user_id, order_uuid ,  total_amount, store_id))
-#         # order_id = cursor.lastrowid
-
-#         # 4. Insert into order_items
-#         for item in items:
-#             cursor.execute("INSERT INTO order_items (order_id, product_id, quantity) VALUES (%s, %s, %s)",
-#                            (order_uuid, item[0], item[2]))
-
-#         # 5. Clear the cart
-#         cursor.execute("DELETE FROM cart WHERE user_id=%s", (user_id,))
-#         conn.commit()
-
-#         return {"message": "Order placed", "order_id": order_uuid, "total_amount": total_amount}
-#     except Exception as e:
-#         conn.rollback()
-#         raise HTTPException(status_code=500, detail=str(e))
-#     finally:
-#         cursor.close()
-#         conn.close()
-
 
 @router.post("/order/checkout")
 def checkout(user_id: int, store_id: int):
